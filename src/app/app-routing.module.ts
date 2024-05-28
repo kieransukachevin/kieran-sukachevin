7d928bf49c0937bf98d2181a173729a4260fef27
Kieran Sukachevin <ksukachevin>
2024-05-28 14:16:57 -0700

    refactor: add @types/estree

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ArtPortfolioComponent } from './art-portfolio/art-portfolio.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ArtPortfolioComponent } from "./art-portfolio/art-portfolio.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 const routes: Routes = [
-  {path: 'art', component: ArtPortfolioComponent},
-  {path: 'projects', component: ProjectsComponent},
-  {path: '', component: ContactComponent}
+  { path: "art", component: ArtPortfolioComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "", component: ContactComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
-export class AppRoutingModule { }
-export const routingComponents = [ArtPortfolioComponent, ContactComponent, ProjectsComponent];
+export class AppRoutingModule {}
+export const routingComponents = [
+  ArtPortfolioComponent,
+  ContactComponent,
+  ProjectsComponent
+];
