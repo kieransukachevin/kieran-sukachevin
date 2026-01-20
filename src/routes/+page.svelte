2dc1daa73fcf4f372adffd23520e746b754fd71b
Kieran Sukachevin <ksukachevin@mhighpoint.com>
2026-01-19 17:32:04 -0800

    feat: add linear gradient to h2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	const startAnimation = () => {};
 </script>
 
 <div class="space-y-8">
-	<h1 class="text-7xl tracking-wide" onmouseenter={startAnimation}>Hello!</h1>
+	<h1 class="text-7xl tracking-wide text-slate-700">Hello!</h1>
 	<div class="h-1 w-20 rounded bg-slate-700"></div>
-	<h2 class="font-serif">
+	<h2 class="gradient animate p-4 font-serif text-gray-900">
 		My name is Kieran Sukachevin. I am a software developer and artist currently living in Southern
 		California.
 	</h2>
@@ -17,3 +16,21 @@
 		(checkout my Linkedin). Feel free to reach out to me via the contact page!
 	</p>
 </div>
+
+<style>
+	.gradient {
+		background-size: 300% 300%;
+		background-image: linear-gradient(0.25turn, #d44d24, #ffffb6, #b4f987);
+	}
+	.animate {
+		animation: animatedBackground 8s linear infinite alternate;
+	}
+	@keyframes animatedBackground {
+		from {
+			background-position-x: 0%;
+		}
+		to {
+			background-position-x: 100%;
+		}
+	}
+</style>
