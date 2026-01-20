204f99171c7915d8d12a3204103d069d39725c25
Kieran Sukachevin <ksukachevin@mhighpoint.com>
2026-01-19 17:36:49 -0800

    fix: relative path navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import NavButton from '$lib/nav-button.svelte';
+	import { resolve } from '$app/paths';
 
 	const { children } = $props();
 	// If you're using a fallback (i.e. SPA mode) you don't need to prerender all
@@ -12,8 +13,8 @@
 
 <div class="h-screen bg-white text-slate-800 transition-all">
 	<nav class="flex items-center gap-6 border-b border-gray-500 px-16 py-4 text-slate-600">
-		<NavButton href="/">About</NavButton>
-		<NavButton href="/contact">Contact</NavButton>
+		<NavButton href={resolve('/')}>About</NavButton>
+		<NavButton href={resolve('/contact')}>Contact</NavButton>
 	</nav>
 	<div class="m-auto max-w-170 space-y-4 pt-12">{@render children()}</div>
 </div>
