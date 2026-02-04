d676f7c7baf401306e1b4b643bc15e1ab4beeaf9
Kieran Sukachevin <ksukachevin@mhighpoint.com>
2026-02-04 15:22:24 -0800

    feat: include resume.css asset as well

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { asset } from '$app/paths';
+
+	// include resume.css asset as well
+	const resumeCss = asset('/resume.css');
 </script>
 
 <iframe src={asset('/resume.html')} class="h-[80vh] w-full" title="Resume" />
